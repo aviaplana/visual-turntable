f80900bed8e843899f83aaab4c3812a0d7666943
aviaplana <pichi.pride@gmail.com>
2015-12-02 14:52:18 +0100

    Implement speed control

--- arduino/turntable/turntable.ino
+++ arduino/turntable/turntable.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#define DEBUG 1
 
+// Analog pins
+#define PIN_POD_SPEED 0
+
+// Digital pins
+#define PIN_MOTOR_SPEED 3 
+
+// Speed value in which the motor stops.
+uint8_t min_speed = 45;
+
+
+void setup() 
+{
+  pinMode(PIN_MOTOR_SPEED, OUTPUT);
+  
+  if (DEBUG) {
+    Serial.begin(9600);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  int motor_speed = map(analogRead(PIN_POD_SPEED), 0, 1024, min_speed, 254);
+  
+  if (DEBUG)
+  {
+    Serial.println(motor_speed);
+  }
+  
+  analogWrite(PIN_MOTOR_SPEED, motor_speed);
+  
 }
